ed6643f4740407849a97fe19d12daa720d057eed
Srinivas Joshi <srinivasjoshi60@gmail.com>
2022-12-10 13:14:17 +0530

    updated contract

--- contracts/erc1155.sol
+++ contracts/erc1155.sol
@@ -3,11 +3,9 @@ pragma solidity ^0.8.11;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
-import "hardhat/console.sol";
 
-contract NFT1155 is ERC1155, Ownable, ERC1155Supply {
+contract NFT1155 is ERC1155, Ownable {
     //contract address goes here and id will be dynamic and will be passed in the _mint function calls
     //example https://ipfs.io/ipfs/QmT51bbxTbSiYGcF2X39sG6DGYyAX2413A1sZfiACMgJGP?filename={id}.json
     //if the if id 1 then https://ipfs.io/ipfs/QmT51bbxTbSiYGcF2X39sG6DGYyAX2413A1sZfiACMgJGP?filename=1.json will return the data that needs to be minted
@@ -15,6 +13,8 @@ contract NFT1155 is ERC1155, Ownable, ERC1155Supply {
 
     mapping(uint256 => string) internal _tokenURIs;
     mapping(uint256 => MarketItem) private idToMarketItem;
+    mapping(address => mapping(uint256 => bool)) private userHasItem;
+
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
     Counters.Counter private _itemsSold;
@@ -23,50 +23,57 @@ contract NFT1155 is ERC1155, Ownable, ERC1155Supply {
         uint256 tokenId;
         address payable creator;
         uint256 price;
+        uint256 amount;
         bool isUpForSale;
+        string tokenUri;
     }
 
     event MarketItemCreated(
         uint256 tokenId,
         address payable creator,
         uint256 price,
+        uint256 amount,
         bool isUpForSale
     );
 
     function mintToken(
         string memory tokenURI,
-        uint256 amount
-        // uint256 price
+        uint256 amount,
+        uint256 price
     ) public returns (uint256) {
+        require(price > 0, "Price must be at least 1 wei");
         uint256 newItemId = _tokenIds.current();
-        _mint(address(this), newItemId, amount+1, "");
+
+        _mint(msg.sender, newItemId, amount, "");
         _setTokenUri(newItemId, tokenURI);
         _tokenIds.increment();
-        //createMarketItem(newItemId, price, amount);
+
+        idToMarketItem[newItemId] = MarketItem(
+            newItemId,
+            payable(msg.sender),
+            price,
+            amount,
+            false,
+            tokenURI
+        );
         return newItemId;
     }
 
     //put the NFT on the market,except one(in js)
-    function createMarketItem(
-        uint256 tokenId,
-        uint256 price,
-        uint256 amount
-    ) private {
-        require(price > 0, "Price must be at least 1 wei");
-        idToMarketItem[tokenId] = MarketItem(
+    function createMarketItem(uint256 tokenId) public {
+        address creator = idToMarketItem[tokenId].creator;
+        uint256 amount = idToMarketItem[tokenId].amount;
+        uint256 price = idToMarketItem[tokenId].price;
+        require(msg.sender==creator,"Not the creator of NFT");
+
+        // setApprovalForAll(address(this), true);
+        // safeTransferFrom(msg.sender, address(this), tokenId, amount, "");
+        idToMarketItem[tokenId].isUpForSale=true;
+        emit MarketItemCreated(
             tokenId,
             payable(msg.sender),
             price,
-            true
-        );
-        setApprovalForAll(address(this), true);
-
-        safeTransferFrom(msg.sender, address(this), tokenId, amount, "");
-
-        emit MarketItemCreated(
-            tokenId,
-            msg.sender,
-            price,
+            amount,
             true
         );
     }
@@ -74,20 +81,23 @@ contract NFT1155 is ERC1155, Ownable, ERC1155Supply {
     /* Creates the sale of a marketplace item */
     /* Transfers ownership of the item, as well as funds between parties */
     function createMarketSale(uint256 tokenId) public payable {
-        uint256 price = idToMarketItem[tokenId].price;
-        address creator = idToMarketItem[tokenId].creator;
-        bool isUpForSale = idToMarketItem[tokenId].isUpForSale;
+        uint256 _price = idToMarketItem[tokenId].price;
+        uint256 _amount = idToMarketItem[tokenId].amount;
+        address _creator = idToMarketItem[tokenId].creator;
+        bool _isUpForSale = idToMarketItem[tokenId].isUpForSale;
 
-        require(isUpForSale,"The item is not up for sale");
+        require(_isUpForSale,"The item is not up for sale");
         require(
-            msg.value == price,
+            msg.value >= _price,
             "Please submit the asking price in order to complete the purchase"
         );
-
-        idToMarketItem[tokenId].isUpForSale = false;
-
-        safeTransferFrom(address(this), msg.sender, tokenId, 1, "");
-        payable(creator).transfer(msg.value);
+        require(!userHasItem[msg.sender][tokenId],"You already own this NFT");
+        userHasItem[msg.sender][tokenId] = true;
+        idToMarketItem[tokenId].amount = _amount-1;
+        _safeTransferFrom(_creator, msg.sender, tokenId, 1, "");
+        
+        (bool sent,) = _creator.call{value:msg.value}("");
+        require(sent,"Eth not sent to creator");
     }
 
     /* Returns all unsold market items */
@@ -113,16 +123,15 @@ contract NFT1155 is ERC1155, Ownable, ERC1155Supply {
         uint256 currentIndex = 0;
 
         for (uint256 i = 0; i < totalItemCount; i++) {
-            if (idToMarketItem[i + 1].owner == msg.sender) {
+            if (userHasItem[msg.sender][i]) {
                 itemCount += 1;
             }
         }
 
         MarketItem[] memory items = new MarketItem[](itemCount);
         for (uint256 i = 0; i < totalItemCount; i++) {
-            if (idToMarketItem[i + 1].owner == msg.sender) {
-                uint256 currentId = i + 1;
-                MarketItem storage currentItem = idToMarketItem[currentId];
+            if (userHasItem[msg.sender][i]) {
+                MarketItem storage currentItem = idToMarketItem[i];
                 items[currentIndex] = currentItem;
                 currentIndex += 1;
             }
@@ -137,16 +146,15 @@ contract NFT1155 is ERC1155, Ownable, ERC1155Supply {
         uint256 currentIndex = 0;
 
         for (uint256 i = 0; i < totalItemCount; i++) {
-            if (idToMarketItem[i + 1].seller == msg.sender) {
+            if (idToMarketItem[i].creator == msg.sender) {
                 itemCount += 1;
             }
         }
 
         MarketItem[] memory items = new MarketItem[](itemCount);
         for (uint256 i = 0; i < totalItemCount; i++) {
-            if (idToMarketItem[i + 1].seller == msg.sender) {
-                uint256 currentId = i + 1;
-                MarketItem storage currentItem = idToMarketItem[currentId];
+            if (idToMarketItem[i].creator == msg.sender) {
+                MarketItem storage currentItem = idToMarketItem[i];
                 items[currentIndex] = currentItem;
                 currentIndex += 1;
             }
@@ -161,27 +169,6 @@ contract NFT1155 is ERC1155, Ownable, ERC1155Supply {
 
     function _setTokenUri(uint256 tokenId, string memory tokenURI) private {
         _tokenURIs[tokenId] = tokenURI;
-    }
-
-    function mintBatch(
-        address to,
-        uint256[] memory ids,
-        uint256[] memory amounts,
-        bytes memory data
-    ) public onlyOwner {
-        _mintBatch(to, ids, amounts, data);
-    }
-
-    // The following functions are overrides required by Solidity.
-    function _beforeTokenTransfer(
-        address operator,
-        address from,
-        address to,
-        uint256[] memory ids,
-        uint256[] memory amounts,
-        bytes memory data
-    ) internal override(ERC1155, ERC1155Supply) {
-        super._beforeTokenTransfer(operator, from, to, ids, amounts, data);
     }
 
     function onERC1155Received(
